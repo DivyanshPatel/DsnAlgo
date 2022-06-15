f0d3419c8753582ffbc6db446c15815054daee6d
Divyansh <nealbay@gmail.com>
2022-06-14 17:12:42 -0700

    Array 2 sum Brute Force

---
+++ Array - 2 sum brute force.js
@@ -0,0 +1,17 @@
+nums=[1,3,7,9,2]
+target = 11
+var twoSum = function(nums,target){
+    for(let p1=0;p1<nums.length;p1++){
+        const numberToFind = target - nums[p1]
+
+        for(let p2=p1+1 ; p2<nums.length; p2++ ){
+            if (numberToFind == nums[p2]){
+                return [p1,p2]
+            }
+        }
+    }
+    return null
+};
+
+
+console.log(twoSum(nums,target));
